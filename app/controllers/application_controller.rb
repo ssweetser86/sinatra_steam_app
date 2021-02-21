f8b65fef1fe416f29bdffd4a6274b2d1ca4d7ac0
Stephen <stephenjsweets@gmail.com>
2021-02-21 15:37:58 -0500

    helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,38 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
   end
 
   get "/" do
     erb :welcome
   end
 
+
+  helpers do
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect "/login"
+      end
+    end
+
+    def redirect_to_home_page
+      redirect to "/games"
+    end
+
+    def redirect_to_consoles
+      redirect to "/consoles"
+    end
+
+  end
 end
